26d2be73482d83ce8cbf15772d408f92da7830a0
ecalasans <ericcalasans@gmail.com>
2017-11-12 08:30:52 -0300

    Construindo fitness

--- genetics.py
+++ genetics.py
@@ -1,24 +1,50 @@
 import numpy as np
+import intervals as intervalo
 
-def generateGene():
+OBS_1X = intervalo.FloatInterval.closed(0.75, 1.25)
+OBS_1Y = intervalo.FloatInterval.closed(0.75, 1.25)
+
+OBS_2X = intervalo.FloatInterval.closed(1.25, 1.75)
+OBS_2Y = intervalo.FloatInterval(3.25, 3.75)
+
+OBS_3X = intervalo.FloatInterval.closed(2.25, 2.75)
+OBS_3Y = intervalo.FloatInterval.closed(2.25, 2.75)
+
+OBS_4X = intervalo.FloatInterval.closed(4.25, 4.75)
+OBS_4Y = intervalo.FloatInterval.closed(1.75, 2.25)
+
+OBS_5X = intervalo.FloatInterval.closed(2.75, 3.25)
+OBS_5Y = intervalo.FloatInterval.closed(0.75, 1.25)
+
+OBS_6X = intervalo.FloatInterval.closed(3.75, 4.25)
+OBS_6Y = intervalo.FloatInterval.closed(2.75, 3.25)
+
+def generateChromosome():
     x = np.random.uniform(0, 5, 1)
     y = np.random.uniform(0, 5, 1)
-    return [round(x[0], 1), round(y[0] ,1)]
+    return np.array((round(x[0], 1), round(y[0], 1)))
 
-def generateChromosome(tam):
-    chromosome = []
+def generateIndividual(tamInd):
+    individuo = []
 
-    for i in range(0, tam):
-        chromosome.append(generateGene())
+    for i in range(0, tamInd):
+        individuo.append(generateChromosome())
 
-    return chromosome
+    return individuo
 
-def generatePopulation(tamPop, tamChrom):
+def generatePopulation(nIndividuals, tamInd):
     pop = []
 
-    for i in range(0, tamPop):
-        pop.append(generateChromosome(tamChrom))
+    for i in range(0, nIndividuals):
+        pop.append(generateIndividual(tamInd))
 
     return pop
+
+def fitness(individuo, target):
+    distPT = []
+
+    for i in individuo[0]:
+        distPT.append(round(np.linalg.norm(i - target), 2))
+
 
 
--- testeGA.py
+++ testeGA.py
@@ -1,10 +1,31 @@
+import numpy as np
 import genetics
+import intervals as intervalo
 
-gene = genetics.generateGene()
+OBS_1X = intervalo.FloatInterval.closed(0.75, 1.25)
+OBS_1Y = intervalo.FloatInterval.closed(0.75, 1.25)
 
-cromossomo = genetics.generateChromosome(6)
+OBS_2X = intervalo.FloatInterval.closed(1.25, 1.75)
+OBS_2Y = intervalo.FloatInterval(3.25, 3.75)
 
-populacao = genetics.generatePopulation(20,4)
+OBS_3X = intervalo.FloatInterval.closed(2.25, 2.75)
+OBS_3Y = intervalo.FloatInterval.closed(2.25, 2.75)
 
-for i in range(len(populacao)):
-    print populacao[i]
+OBS_4X = intervalo.FloatInterval.closed(4.25, 4.75)
+OBS_4Y = intervalo.FloatInterval.closed(1.75, 2.25)
+
+OBS_5X = intervalo.FloatInterval.closed(2.75, 3.25)
+OBS_5Y = intervalo.FloatInterval.closed(0.75, 1.25)
+
+OBS_6X = intervalo.FloatInterval.closed(3.75, 4.25)
+OBS_6Y = intervalo.FloatInterval.closed(2.75, 3.25)
+
+populacao = genetics.generatePopulation(1,2)
+
+# print populacao[0]
+#
+# print populacao[0][0]-populacao[0][1]
+#
+# dist = np.linalg.norm(populacao[0][0]-populacao[0][1])
+#
+# print dist
