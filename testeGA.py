a9c1c300ada85bbd0780ca2247fb3c65b079e8c2
ecalasans <ericcalasans@gmail.com>
2017-11-12 10:30:28 -0300

    Continuando fitness

--- testeGA.py
+++ testeGA.py
@@ -1,31 +1,29 @@
 import numpy as np
 import genetics
-import intervals as intervalo
 
-OBS_1X = intervalo.FloatInterval.closed(0.75, 1.25)
-OBS_1Y = intervalo.FloatInterval.closed(0.75, 1.25)
 
-OBS_2X = intervalo.FloatInterval.closed(1.25, 1.75)
-OBS_2Y = intervalo.FloatInterval(3.25, 3.75)
+OBS_1X = np.linspace(0.75, 1.25, num=20)
+OBS_1Y = np.linspace(0.75, 1.25, num=20)
 
-OBS_3X = intervalo.FloatInterval.closed(2.25, 2.75)
-OBS_3Y = intervalo.FloatInterval.closed(2.25, 2.75)
+OBS_2X = np.linspace(1.25, 1.75, num=20)
+OBS_2Y = np.linspace(3.25, 3.75, num=20)
 
-OBS_4X = intervalo.FloatInterval.closed(4.25, 4.75)
-OBS_4Y = intervalo.FloatInterval.closed(1.75, 2.25)
+OBS_3X = np.linspace(2.25, 2.75, num=20)
+OBS_3Y = np.linspace(2.25, 2.75, num=20)
 
-OBS_5X = intervalo.FloatInterval.closed(2.75, 3.25)
-OBS_5Y = intervalo.FloatInterval.closed(0.75, 1.25)
+OBS_4X = np.linspace(4.25, 4.75, num=20)
+OBS_4Y = np.linspace(1.75, 2.25, num=20)
 
-OBS_6X = intervalo.FloatInterval.closed(3.75, 4.25)
-OBS_6Y = intervalo.FloatInterval.closed(2.75, 3.25)
+OBS_5X = np.linspace(2.75, 3.25, num=20)
+OBS_5Y = np.linspace(0.75, 1.25, num=20)
 
-populacao = genetics.generatePopulation(1,2)
+OBS_6X = np.linspace(3.75, 4.25, num=20)
+OBS_6Y = np.linspace(2.75, 3.25, num=20)
 
-# print populacao[0]
-#
-# print populacao[0][0]-populacao[0][1]
-#
-# dist = np.linalg.norm(populacao[0][0]-populacao[0][1])
-#
-# print dist
+obstaculos = [[OBS_1X, OBS_1Y], [OBS_2X, OBS_2Y], [OBS_3X, OBS_3Y],
+                       [OBS_4X, OBS_4Y], [OBS_5X, OBS_5Y], [OBS_6X, OBS_6Y]]
+
+
+ponto = [1.2, 1.6]
+
+print OBS_1Y
